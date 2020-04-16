a4f6753e58f0c9682b91b8db310584d7ec2bb72f
Megan <megan.miller@smoldev.dev>
2020-04-15 21:44:02 -0500

    removing extraneous jquery code

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,11 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-exports.onCreateWebpackConfig = ({ actions }) => {
-  const { setWebpackConfig } = actions
-  setWebpackConfig({
-    externals: {
-      jquery: "jQuery", // important: 'Q' capitalized
-    },
-  })
-}
