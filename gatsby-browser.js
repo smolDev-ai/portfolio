4a715dca17a1bf2a89b7690dd226b7823e865d8b
Megan <megan.miller@smoldev.dev>
2020-04-16 00:17:02 -0500

    Jquery working on development. Fingers crossed for deployment

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,8 @@
  */
 
 // You can delete this file if you're not using it
-import $ from "jquery"
+const $ = require("jquery")
+
 export const onInitialClientRender = () => {
   $(document).ready(function() {
     console.log("The answer is don't think about it!")
