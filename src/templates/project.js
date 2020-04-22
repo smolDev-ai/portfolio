ea0b2f9eca3ce7716e87b683327c5750f86b9c38
Megan <megan.miller@smoldev.dev>
2020-04-22 16:42:08 -0500

    Modifying project details styles.

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 
-import Layout from "../components/Layout"
+import Layout from "/src/components/Layout"
 import Img from "gatsby-image"
 
 export const query = graphql`
