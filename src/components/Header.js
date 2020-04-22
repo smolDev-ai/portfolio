08dd916c7d624f8549a57b69efd86e73006163a2
Megan <megan.miller@smoldev.dev>
2020-04-22 16:52:34 -0500

    Fixing Nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,22 +20,22 @@ const Header = ({ siteTitle }) => {
             style={{ height: "120px", objectFit: "cover" }}
           />
           <h1 className="text-light">
-            <a href="/">Megan Miller</a>
+            <Link to="/">Megan Miller</Link>
           </h1>
           <div className="social-links mt-3 text-center">
-            <a href="#" className="twitter">
+            <a href="#" className="twitter" alt="twitter">
               <i className="bx bxl-twitter"></i>
             </a>
-            <a href="#" className="facebook">
+            <a href="#" className="facebook" alt="medium">
               <i className="bx bxl-medium"></i>
             </a>
-            <a href="#" className="instagram">
+            <a href="#" className="instagram" alt="dev-to">
               <i className="bx bxl-dev-to"></i>
             </a>
-            <a href="#" className="google-plus">
+            <a href="#" className="google-plus" alt="github">
               <i className="bx bxl-github"></i>
             </a>
-            <a href="#" className="linkedin">
+            <a href="#" className="linkedin" alt="linkedin">
               <i className="bx bxl-linkedin"></i>
             </a>
           </div>
@@ -43,24 +43,24 @@ const Header = ({ siteTitle }) => {
         <nav class="nav-menu">
           <ul>
             <li>
-              <a href="#">
+              <Link to="/">
                 <i class="bx bx-home"></i> <span>Home</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#about">
+              <Link to="#about">
                 <i class="bx bx-user"></i> <span>About</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#skills">
+              <Link to="#skills">
                 <i class="bx bx-server"></i> <span>Skills</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#portfolio">
+              <Link to="#portfolio">
                 <i class="bx bx-book-content"></i> <span>Portfolio</span>
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
